ce105527cdbc520fac4d20ea14123cfec70a61fa
Marko Nikolić <116906156+marko-nikolic01@users.noreply.github.com>
2024-07-01 15:57:05 +0200

    feat: Added Airport model.

---
+++ model/Airport.go
@@ -0,0 +1,13 @@
+package model
+
+type Airport struct {
+	Name         string
+	IATA         string
+	Latitude     string
+	Longitude    string
+	Elevation    string
+	Continent    string
+	Country      string
+	Region       string
+	Municipality string
+}
