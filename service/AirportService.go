fbae591641bdf85e2b6861830c4839d1da355a1a
Marko Nikolić <116906156+marko-nikolic01@users.noreply.github.com>
2024-07-13 00:30:33 +0200

    frst: Airport sorting.

--- service/AirportService.go
+++ service/AirportService.go
@@ -3,6 +3,7 @@ package service
 import (
 	"airport-API/model"
 	"airport-API/repository"
+	"sort"
 	"strings"
 )
 
@@ -19,22 +20,29 @@ func NewAirportService(repo repository.AirportRepository) AirportService {
 }
 
 func (s *airportService) GetAirports(search string) ([]model.Airport, error) {
-	if search == "" {
-		return s.repo.FindAll()
-	}
-
-	search = strings.ToLower(search)
 	airports, err := s.repo.FindAll()
 	if err != nil {
 		return nil, err
 	}
 
+	if search == "" {
+		sort.Slice(airports, func(i, j int) bool {
+			return airports[i].IATA < airports[j].IATA
+		})
+		return airports, nil
+	}
+
+	search = strings.ToLower(search)
 	var result []model.Airport
 	for _, airport := range airports {
 		if strings.Contains(strings.ToLower(airport.Name), search) || strings.Contains(strings.ToLower(airport.IATA), search) {
 			result = append(result, airport)
 		}
 	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].IATA < result[j].IATA
+	})
 
 	return result, nil
 }
